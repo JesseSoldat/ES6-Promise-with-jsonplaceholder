2426b8cdb938eb34033e1e785380a71f28f9a26c
jesse soldat <jsoldat@hotmail.com>
2016-08-15 22:13:15 +0700

    added some ajax requests

--- main.js
+++ main.js
@@ -24,8 +24,36 @@ function getData(method, url) {
 	});
 }
 
-getData('GET','https://jsonplaceholder.typicode.com/todos').then(function(data){
-	console.log(data);
-}).catch(function(err){
-	console.log(err);
+// getData('GET','https://jsonplaceholder.typicode.com/todos').then(function(data){
+// 	console.log(data);
+// }).catch(function(err){
+// 	console.log(err);
+// });
+
+
+// $.ajax('http://jsonplaceholder.typicode.com/posts/1', {
+//   method: 'GET'
+// }).then(function(data) {
+//   console.log(data);
+// });
+
+// $.ajax('http://jsonplaceholder.typicode.com/posts', {
+//   method: 'GET'
+// }).then(function(data) {
+//   console.log(data);
+// });
+
+
+// POST adds a random id to the object sent
+$.ajax('http://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  data: {
+    title: 'My Ajax Adventure',
+    body: 'Playing around with AJAX',
+    userId: 1,
+    author: 'Jesse Soldat'
+  }
+}).then(function(data) {
+  console.log(data);
 });
+
