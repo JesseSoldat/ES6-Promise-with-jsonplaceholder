8808ee6aab6330df74e9e7595be1da2cc8c03376
jesse soldat <jsoldat@hotmail.com>
2016-08-15 22:03:08 +0700

    es6 promise

---
+++ main.js
@@ -0,0 +1,31 @@
+//https://jsonplaceholder.typicode.com/todos
+
+function getData(method, url) {
+	return new Promise(function(resolve, reject){
+		var xhr = new XMLHttpRequest();
+		xhr.open(method, url);
+		xhr.onload = function(){
+			if(this.status >= 200 && this.status < 300) {
+				resolve(xhr.response);
+			} else {
+				reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+			}
+		};
+		xhr.onerror = function(){
+			reject({
+					status: this.status,
+					statusText: xhr.statusText
+				});
+		};
+		xhr.send();
+	});
+}
+
+getData('GET','https://jsonplaceholder.typicode.com/todos').then(function(data){
+	console.log(data);
+}).catch(function(err){
+	console.log(err);
+});
